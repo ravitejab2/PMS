807987248d9cf72c3496aa73b77de33732e4b890
ctteam99 <ctteam99@gmail.com>
2022-10-13 14:19:18 +0530

    Environment api urls Added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,14 @@
 export const environment = {
   production: true,
+  
   baseApiUrl:'https://localhost:44362/api/admin',
+  baseAccountUrl:'https://localhost:44362/api/account',
   patientApiUrl:'https://localhost:44345/api/PatientDetails',
   emergencyContactApiUrl:'https://localhost:44345/api/EmergencyContact',
   allergyApiUrl:'https://localhost:44345/api/allergies',
   dignosisApiUrl:'https://localhost:44345/api/PatientDiagnosisMedication',
+  appointmentApiUrl:'https://localhost:44352/api/appointments',
   visitVitalUrl:'https://localhost:44345/api/Vitals',
-  patient_diagnosisUrl:'https://localhost:44345/api/PatientDiagnosisMedication'
+  patient_diagnosisUrl:'https://localhost:44345/api/PatientDiagnosisMedication',
+  inboxApiUrl:'https://localhost:44340/api/notes'
 };
